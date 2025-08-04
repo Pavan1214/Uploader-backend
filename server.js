fd8c4c049e76cbc445bae82088fa1595cc9bde49
pavan aitagoni <165299332+Pavan1214@users.noreply.github.com>
2025-08-04 20:38:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB Atlas
+// Connect to MongoDB Atlass
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
